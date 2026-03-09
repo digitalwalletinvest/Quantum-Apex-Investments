85c1089f2a5758c091f64d4245f67bce25cfc626
digitalwalletinvest <digitalinvestinfo@gmail.com>
2026-03-09 16:07:18 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE="quantum-apex-v1";
+
+const assets=[
+"/",
+"/index.html"
+];
+
+self.addEventListener("install",e=>{
+e.waitUntil(
+caches.open(CACHE).then(cache=>cache.addAll(assets))
+);
+});
+
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request).then(res=>{
+return res||fetch(e.request);
+})
+);
+});
