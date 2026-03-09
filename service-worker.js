5974c10eefb692a790503215f4a24bff3aeb8812
digitalwalletinvest <digitalinvestinfo@gmail.com>
2026-03-09 18:35:06 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,50 @@
-const CACHE="quantum-apex-v1";
-
-const assets=[
-"/",
-"/index.html"
+const CACHE_NAME = 'quantum-apex-cache-v1';
+const urlsToCache = [
+  '/Quantum-Apex-Investments/',              // index.html
+  '/Quantum-Apex-Investments/index.html',
+  '/Quantum-Apex-Investments/icons/icon-152.png',
+  '/Quantum-Apex-Investments/icons/icon-192.png',
+  '/Quantum-Apex-Investments/icons/icon-512.png',
+  '/Quantum-Apex-Investments/icons/splash.png',
+  '/Quantum-Apex-Investments/manifest.json',
+  '/Quantum-Apex-Investments/style.css',      // if you have external CSS
+  '/Quantum-Apex-Investments/script.js'       // if you have external JS
 ];
 
-self.addEventListener("install",e=>{
-e.waitUntil(
-caches.open(CACHE).then(cache=>cache.addAll(assets))
-);
+// Install event - caching files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Caching app assets...');
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener("fetch",e=>{
-e.respondWith(
-caches.match(e.request).then(res=>{
-return res||fetch(e.request);
-})
-);
+// Activate event - clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Removing old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch event - serve cached assets first
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
