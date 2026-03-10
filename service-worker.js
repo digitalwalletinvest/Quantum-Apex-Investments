adb42c9c1d5dfec6614fca89cd4255cfa84f64ab
digitalwalletinvest <digitalinvestinfo@gmail.com>
2026-03-10 08:09:33 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,50 +1,51 @@
-const CACHE_NAME = 'quantum-apex-cache-v1';
+const CACHE_NAME = "quantum-apex-cache-v1";
+
 const urlsToCache = [
-  '/Quantum-Apex-Investments/',              // index.html
-  '/Quantum-Apex-Investments/index.html',
-  '/Quantum-Apex-Investments/icons/icon-152.png',
-  '/Quantum-Apex-Investments/icons/icon-192.png',
-  '/Quantum-Apex-Investments/icons/icon-512.png',
-  '/Quantum-Apex-Investments/icons/splash.png',
-  '/Quantum-Apex-Investments/manifest.json',
-  '/Quantum-Apex-Investments/style.css',      // if you have external CSS
-  '/Quantum-Apex-Investments/script.js'       // if you have external JS
+  "/Quantum-Apex-Investments/",
+  "/Quantum-Apex-Investments/index.html",
+  "/Quantum-Apex-Investments/login.html",
+  "/Quantum-Apex-Investments/register.html",
+  "/Quantum-Apex-Investments/manifest.json",
+
+  "/Quantum-Apex-Investments/icons/icon-72.png",
+  "/Quantum-Apex-Investments/icons/icon-96.png",
+  "/Quantum-Apex-Investments/icons/icon-128.png",
+  "/Quantum-Apex-Investments/icons/icon-144.png",
+  "/Quantum-Apex-Investments/icons/icon-152.png",
+  "/Quantum-Apex-Investments/icons/icon-192.png",
+  "/Quantum-Apex-Investments/icons/icon-512.png"
 ];
 
-// Install event - caching files
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
-        console.log('Caching app assets...');
         return cache.addAll(urlsToCache);
       })
-      .then(() => self.skipWaiting())
   );
 });
 
-// Activate event - clean up old caches
-self.addEventListener('activate', event => {
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+
   event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            console.log('Removing old cache:', cache);
-            return caches.delete(cache);
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
           }
         })
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-// Fetch event - serve cached assets first
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+      );
     })
   );
 });
